020a52dea0f0576ca039a8ce34414b285a2b75b8
seonhoseo <122855132+seonhoseo@users.noreply.github.com>
2023-01-19 17:14:01 +0900

    [fail]pagination try

--- src/main/java/egovframework/gjdm/controller/DimCalendarController.java
+++ src/main/java/egovframework/gjdm/controller/DimCalendarController.java
@@ -1,15 +1,12 @@
 package egovframework.gjdm.controller;
 
 import java.util.List;
-
 import javax.annotation.Resource;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import egovframework.gjdm.service.DimCalendarService;
 import egovframework.gjdm.vo.DimCalendarVO;
 
@@ -25,7 +22,7 @@ public class DimCalendarController {
 	public String selectDimCalendarList(Model model) throws Exception {
 //		logger.debug("calendarList......calendarList......calendarList......");
 		List<DimCalendarVO> list = dimCalendarService.selectDimCalendarList();
-		
+
 		model.addAttribute("dimCalendarList", list);
 		
 		return "standard_manage/calendar";
