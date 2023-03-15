b7eea2e0ebf3e248e40b162a5836e6abe01a14d5
yeji kim <awesomekimn@gmail.com>
2023-03-15 17:38:26 +0900

    district -> area.jsp로 변경 및 업데이트

--- src/main/java/egovframework/gjdm/vo/DimDistrictVO.java
+++ src/main/java/egovframework/gjdm/vo/DimDistrictVO.java
@@ -8,8 +8,11 @@ public class DimDistrictVO {
 	private int districtId;
 	private int nationId;
 	private String distLvl1;
+	private String distLvl1Cd;
+	private String distLvl1CdSync;
 	private String distLvl2;
-	private String distLvl3;
+	private String distLvl2Cd;
+	private String distLvl2CdSync;
 	private Date rgtrDt;
 	private String rgtrId;
 	private Date updtDt;
@@ -33,17 +36,35 @@ public class DimDistrictVO {
 	public void setDistLvl1(String distLvl1) {
 		this.distLvl1 = distLvl1;
 	}
+	public String getDistLvl1Cd() {
+		return distLvl1Cd;
+	}
+	public void setDistLvl1Cd(String distLvl1Cd) {
+		this.distLvl1Cd = distLvl1Cd;
+	}
+	public String getDistLvl1CdSync() {
+		return distLvl1CdSync;
+	}
+	public void setDistLvl1CdSync(String distLvl1CdSync) {
+		this.distLvl1CdSync = distLvl1CdSync;
+	}
 	public String getDistLvl2() {
 		return distLvl2;
 	}
 	public void setDistLvl2(String distLvl2) {
 		this.distLvl2 = distLvl2;
 	}
-	public String getDistLvl3() {
-		return distLvl3;
+	public String getDistLvl2Cd() {
+		return distLvl2Cd;
 	}
-	public void setDistLvl3(String distLvl3) {
-		this.distLvl3 = distLvl3;
+	public void setDistLvl2Cd(String distLvl2Cd) {
+		this.distLvl2Cd = distLvl2Cd;
+	}
+	public String getDistLvl2CdSync() {
+		return distLvl2CdSync;
+	}
+	public void setDistLvl2CdSync(String distLvl2CdSync) {
+		this.distLvl2CdSync = distLvl2CdSync;
 	}
 	public Date getRgtrDt() {
 		return rgtrDt;
@@ -69,4 +90,5 @@ public class DimDistrictVO {
 	public void setUpdtId(String updtId) {
 		this.updtId = updtId;
 	}
+	
 }
